2dd7deb2aae5280ee0eca32161189a895c4d354a
yaw.o.k <obengky72@gmail.com>
2023-05-19 10:10:54 +0000

    Add models and register admin models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin
+from main import models
 
-# Register your models here.
+admin.site.register(models.Club)
+admin.site.register(models.Venue)
+admin.site.register(models.Membership)
+
+@admin.register(models.Event)
+class EventAdmin(OSMGeoAdmin):
+    list_display = ["name", "slug"]
--- main/models.py
+++ main/models.py
@@ -1,3 +1,48 @@
-from django.db import models
+from django.contrib.gis.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Venue(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=48)
+    description = models.TextField()
+    polygon = models.PolygonField()
+    
+    def __str__(self):
+        return self.name
+
+
+class Club(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=48)
+    description = models.TextField(null=True, blank=True)
+    active = models.BooleanField(default=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    merchant_access_token = models.CharField()
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.CharField(max_length=48)
+    description = models.TextField(null=True, blank=True)
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Membership(models.Model):
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
+    active = models.BooleanField(default=True)
+    subscriber = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer_id = models.CharField()
+    subscription_id = models.CharField()
+
+    def __str__(self):
+        return f"{self.subscriber} - {self.club}"
